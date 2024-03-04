ee09d11430a1e1e4ec8aeebeb0b94654c53f2b24
joshuabutler <butlerja.nhs@gmail.com>
2024-03-04 12:58:09 -0500

    add destination model to view and define urls

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 
+
+class Destination:
+  def __init__(self, name, country, description):
+    self.name = name
+    self.country = country
+    self.description = description
+    
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
