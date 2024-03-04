6cd38d513526c7a122b2d3d54b379b026f58272a
joshuabutler <butlerja.nhs@gmail.com>
2024-03-04 13:17:50 -0500

    add index view for destinations with css

--- main_app/views.py
+++ main_app/views.py
@@ -8,9 +8,17 @@ class Destination:
     self.description = description
     
 
+destinations = [
+  Destination('Universal Studios', 'United States', 'Super fun place to go! But do not go during the summer because you could potentially melt.'),
+  Destination('Disney', 'United States', 'Apparently it is frowned upon to jump out of the boats on water rides here. Ridiculous.'),
+]
+
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def destination_index(request):
+  return render(request, 'destinations/index.html', {'destinations': destinations})
