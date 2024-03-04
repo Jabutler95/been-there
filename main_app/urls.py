5f8c6519de316a3034da45e7aeaa8a76bb8af73a
joshuabutler <butlerja.nhs@gmail.com>
2024-03-04 16:03:03 -0500

    update detail template to include edit and delete button

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
   path('destinations/', views.destination_index, name='destination-index'),
   path('destinations/<int:destination_id>/', views.destination_detail, name='destination-detail'),
   path('destinations/create/', views.DestinationCreate.as_view(), name='destination-create'),
+  path('destinations/<int:pk>/update/', views.DestinationUpdate.as_view(), name='destination-update'),
+  path('destinations/<int:pk>/delete/', views.DestinationDelete.as_view(), name='destination-delete'),
 ]
