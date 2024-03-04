465d0d9f2dc82df6f76605958654f972946301c4
joshuabutler <butlerja.nhs@gmail.com>
2024-03-04 14:28:01 -0500

    add destination model

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'), 
-  path('cats/', views.cat_index, name='cat-index'),
+  path('destinations/', views.destination_index, name='destination-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,17 +1,5 @@
 from django.shortcuts import render
-
-
-class Destination:
-  def __init__(self, name, country, description):
-    self.name = name
-    self.country = country
-    self.description = description
-    
-
-destinations = [
-  Destination('Universal Studios', 'United States', 'Super fun place to go! But do not go during the summer because you could potentially melt.'),
-  Destination('Disney', 'United States', 'Apparently it is frowned upon to jump out of the boats on water rides here. Ridiculous.'),
-]
+from .models import Destination
 
 # Create your views here.
 def home(request):
@@ -21,4 +9,5 @@ def about(request):
   return render(request, 'about.html')
 
 def destination_index(request):
+  destinations = Destination.objects.all()
   return render(request, 'destinations/index.html', {'destinations': destinations})
