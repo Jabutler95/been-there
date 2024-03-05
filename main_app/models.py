c93ddd756fe0b0e0aa92e947d196d52dfc3467bc
joshuabutler <butlerja.nhs@gmail.com>
2024-03-05 13:46:10 -0500

    update destination to include initial visit and user

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.urls import reverse
+from django.contrib.auth.models import User
+from datetime import date
 
 
 
@@ -9,6 +11,9 @@ class Destination(models.Model):
   name = models.CharField(max_length=100)
   country = models.CharField(max_length=100)
   description = models.CharField(max_length=100)
+  first_visit = models.DateField(default=date.today)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
   def __str__(self):
     return self.name
@@ -18,7 +23,7 @@ class Destination(models.Model):
   
 class Visit(models.Model):
   date = models.DateField('Visit Date')
-  
+
 
   destination = models.ForeignKey(Destination, on_delete=models.CASCADE)
 
